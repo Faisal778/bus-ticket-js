6f96c891222b534708cb6e02573465465a3f1957
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-02-22 17:04:55 +0600

    ticket buying functionality added

--- script.js
+++ script.js
@@ -1,1 +1,87 @@
 console.log('script.js connected');
+
+
+const seatBtn = document.getElementsByClassName('seat-btn');
+
+let seatCounter = 0; 
+for (const btn of seatBtn){
+    btn.addEventListener('click', function(event){
+      const seatName = event.target.innerText;
+      let price = document.getElementById('price').innerText;
+      price = parseInt(price);
+
+      event.target.setAttribute("disabled", true);
+      seatCounter = seatCounter + 1;
+      if (seatCounter > 4){
+        alert('You have reached seat limit');
+      } else{
+
+        document.getElementById('seat-count').innerText = seatCounter ;
+        document.getElementById('seat-remaining').innerText = 40 - seatCounter;
+
+      event.target.style.backgroundColor = "#1DD100";
+      event.target.style.color = "white";
+
+
+      console.log(seatName, price);
+
+      const appendDiv = document.getElementById('total-price');
+      const div = document.createElement('div');
+      div.classList.add("flex");
+      div.classList.add("justify-between");
+      div.classList.add("p-4");
+
+      const p1 = document.createElement('p');
+      const p2 = document.createElement('p');
+      const p3 = document.createElement('p');
+
+      const economy = "Economy"
+      p1.innerText = seatName;
+      p2.innerText = economy;
+      p3.innerText = price;
+
+      div.appendChild(p1);
+      div.appendChild(p2);
+      div.appendChild(p3);
+
+      appendDiv.appendChild(div);
+
+    
+
+        //calculate total //
+         
+      const previousSum = document.getElementById('total-sum').innerText;
+      const intSum = parseInt (previousSum);
+      const sum = intSum + price;
+      document.getElementById('total-sum').innerText = sum;
+    
+
+        //update grand total with no coupon//
+        document.getElementById('grand-total').innerText = sum;
+    
+      }
+
+    });
+}
+
+function updateGrandtotal(id){
+
+    const totalSum = document.getElementById('total-sum').innerText;
+    const intSum = parseInt (totalSum);
+    const couponCode = document.getElementById('coupon-btn').value;
+     let grandTotal = document.getElementById('grand-total');
+    if(couponCode === "NEW15"){
+        if (seatCounter >= 4){
+        document.getElementById('coupon').style.display = "none";
+        }
+
+        grandTotal = intSum - (intSum*.15);
+        console.log(grandTotal);
+        document.getElementById('grand-total').innerText = grandTotal;
+    } else{
+        alert('Please Enter a valid coupon');
+    }
+
+    
+    
+}
